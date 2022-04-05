f107d51fc568464ea1112d83503f1a2ecc70aa85
rodrigopastorfelis <84048157+rodrigopastorfelis@users.noreply.github.com>
2022-04-05 16:46:36 -0300

    priomeiro

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace AulasDescomplica
+{
+  internal class Program
+  {
+    public static void Main(string[] args)
+    {
+      for (int i = 0; i < 5; i++)
+      {
+        int nota1 = int.Parse(Console.ReadLine());
+        int nota2 = int.Parse(Console.ReadLine());
+
+        float media = (nota1 + 2 * nota2) / 3;
+
+        if (media>=6)
+        {
+          Console.WriteLine("Passou");
+        }
+        else
+        {
+          Console.WriteLine("não passou");
+        }
+      }
+ 
+    }
+  }
+}
